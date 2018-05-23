a866751464c0ebcaa6585e57f104568f62b40bb8
Matt Krueger <mkrueger@rstms.net>
2018-05-22 21:30:35 -0500

    fixed random seed bug; reformatted

--- intermit.c
+++ intermit.c
@@ -15,36 +15,37 @@ int main(int argc, char *argv[])
     int i;
     int len;
 
-    fprintf(stderr, "intermit testing mode inserting random pauses in child program's output\n");
+    fprintf(stderr,
+	    "intermit testing mode inserting random pauses in child program's output\n");
 
-    int delay=1000;
-    int percent=0;
-    int seed=time(NULL);
-    int verbose=0;
+    int delay = 1000;
+    int percent = 0;
+    int seed = time(NULL);
+    int verbose = 0;
 
     if (argc < 1) {
 	fprintf(stderr, "Usage: %s <command>\n", argv[0]);
 	exit(EXIT_FAILURE);
     }
 
-    while(argv[1][0] == '-') {
-      switch(argv[1][1]) {
-        case 's':
-	    seed = atoi(argv[1]+2);
-	    srandom(seed);
+    while (argv[1][0] == '-') {
+	switch (argv[1][1]) {
+	case 's':
+	    seed = atoi(argv[1] + 2);
 	    break;
-        case 'p':
-	    percent = atoi(argv[1]+2);
+	case 'p':
+	    percent = atoi(argv[1] + 2);
 	    break;
 	case 'd':
-	    delay  = atoi(argv[1]+2);
+	    delay = atoi(argv[1] + 2);
 	    break;
 	case 'v':
 	    verbose = 1;
-      }
-      argv++;
+	}
+	argv++;
     }
 
+    srandom(seed);
     fprintf(stderr, "seed: %d\n", seed);
     fprintf(stderr, "percent: %d\n", percent);
     fprintf(stderr, "delay: %d\n", delay);
@@ -83,12 +84,12 @@ int main(int argc, char *argv[])
 
     if (cpid == 0) {		/* Child reads from p2c, writes to c2p */
 	// child
-	if(close(pipep2c[1])==-1){	/* Close unused write end of p2c pipe */
+	if (close(pipep2c[1]) == -1) {	/* Close unused write end of p2c pipe */
 	    perror("close");
 	    exit(EXIT_FAILURE);
 	}
 
-	if(close(pipec2p[0])==-1){/* Close unused read end of c2p pipe */
+	if (close(pipec2p[0]) == -1) {	/* Close unused read end of c2p pipe */
 	    perror("close");
 	    exit(EXIT_FAILURE);
 	}
@@ -97,8 +98,8 @@ int main(int argc, char *argv[])
 	    perror("dup2");
 	    exit(EXIT_FAILURE);
 	}
-	
-	if(close(pipep2c[0]) == -1) {
+
+	if (close(pipep2c[0]) == -1) {
 	    perror("close");
 	    exit(EXIT_FAILURE);
 	}
@@ -108,7 +109,7 @@ int main(int argc, char *argv[])
 	    exit(EXIT_FAILURE);
 	}
 
-	if(close(pipec2p[1]) == -1) {
+	if (close(pipec2p[1]) == -1) {
 	    perror("close");
 	    exit(EXIT_FAILURE);
 	}
@@ -119,13 +120,13 @@ int main(int argc, char *argv[])
 	}
     } else {			/* Parent writes to p2c, reads from c2p */
 	// parent
-		/* Close unused read end of p2c pipe */
-	if(close(pipep2c[0])==-1){
+	/* Close unused read end of p2c pipe */
+	if (close(pipep2c[0]) == -1) {
 	    perror("close");
 	    exit(EXIT_FAILURE);
 	}
-		/* Close unused write end of c2p pipe */
-	if(close(pipec2p[1])==-1){
+	/* Close unused write end of c2p pipe */
+	if (close(pipec2p[1]) == -1) {
 	    perror("close");
 	    exit(EXIT_FAILURE);
 	}
@@ -143,17 +144,17 @@ int main(int argc, char *argv[])
 	while (stdin_active || child_active) {
 	    /* Watch stdin (fd 0) and c2p pipe for input. */
 	    FD_ZERO(&rfds);
-	    if(stdin_active)
-	        FD_SET(STDIN_FILENO, &rfds);
-	    if(child_active)
-	        FD_SET(pipec2p[0], &rfds);
+	    if (stdin_active)
+		FD_SET(STDIN_FILENO, &rfds);
+	    if (child_active)
+		FD_SET(pipec2p[0], &rfds);
 
 	    /* Watch stdin (fd 0) and c2p pipe for errors. */
 	    FD_ZERO(&efds);
-	    if(stdin_active)
-	        FD_SET(STDIN_FILENO, &efds);
-	    if(child_active)
-	        FD_SET(pipec2p[0], &efds);
+	    if (stdin_active)
+		FD_SET(STDIN_FILENO, &efds);
+	    if (child_active)
+		FD_SET(pipec2p[0], &efds);
 
 	    retval = select(nfds, &rfds, NULL, &efds, NULL);
 	    /* Don't rely on the value of tv now! */
@@ -173,24 +174,24 @@ int main(int argc, char *argv[])
 			}
 		    } else if (len == 0) {
 			stdin_active = 0;
-			if(close(pipep2c[1])==-1) {
+			if (close(pipep2c[1]) == -1) {
 			    perror("close");
 			    exit(EXIT_FAILURE);
 			}
 		    }
 		}
-	        if (FD_ISSET(pipec2p[0], &rfds)) {
+		if (FD_ISSET(pipec2p[0], &rfds)) {
 		    // read c2p, write to stdout
 		    if ((len = read(pipec2p[0], buf, sizeof(buf))) == -1) {
 			perror("pipec2p read");
 			exit(EXIT_FAILURE);
 		    } else if (len > 0) {
 			if (random() % 100 <= percent) {
-			    if(verbose)
-			        fprintf(stderr, "pause\n");
+			    if (verbose)
+				fprintf(stderr, "pause\n");
 			    usleep(delay * 1000);
-			    if(verbose)
-			        fprintf(stderr, "resume\n");
+			    if (verbose)
+				fprintf(stderr, "resume\n");
 			}
 			if (write(STDOUT_FILENO, buf, len) == -1) {
 			    perror("write pipep2c");
@@ -198,13 +199,13 @@ int main(int argc, char *argv[])
 			}
 		    } else if (len == 0) {
 			child_active = 0;
-			if(close(STDOUT_FILENO)==-1){
+			if (close(STDOUT_FILENO) == -1) {
 			    perror("close");
 			    exit(EXIT_FAILURE);
 			}
 		    }
 		}
-	       	if (FD_ISSET(STDIN_FILENO, &efds)) {
+		if (FD_ISSET(STDIN_FILENO, &efds)) {
 		    perror("stdin exception");
 		    exit(EXIT_FAILURE);
 		}
